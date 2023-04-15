6314fed8ca833b499a8af8b614dbffe3f5a2bd15
YAMAMI Taro <mitarothanken@gmail.com>
2023-04-15 22:14:09 +0900

    truffle を compose に追加

--- geth-node/boot-with-clef.sh
+++ geth-node/boot-with-clef.sh
@@ -7,4 +7,5 @@ geth -networkid 50155 \
     --syncmode full \
     --mine \
     --miner.etherbase $(cat /root/etherbase) \
-    --signer http://$(hostname -i)0:8550
+    --signer http://$(hostname -i)0:8550 \
+    --http --http.addr $(hostname -i) --http.vhosts node1,node2,node3 --graphql --ws
