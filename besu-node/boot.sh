c27ecc1b3476de50942c8125e9a29fd162938bfc
YAMAMI Taro <mitarothanken@gmail.com>
2023-04-23 02:11:08 +0900

    動作を安定させるため、奇数台からなるネットワークとする
    Clique は多数決（要確認）。
    ということは、偶数台だと分裂が確定してしまう可能性がある。
    
    一方、奇数台で構成した場合、過半数が成立したネットワークが
    かつ（はず）なので、起動タイミングもゆるくできる（起動タイミングを
    ずらさなくてもよくなる）はず。

--- besu-node/boot.sh
+++ besu-node/boot.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-sleep $1
 besu --genesis-file=genesis.json \
     --data-path=data \
-    --bootnodes=$(cat geth-node1-enode),$(cat geth-node2-enode) \
+    --bootnodes=$(cat geth-node1-enode),$(cat geth-node2-enode),$(cat geth-node3-enode) \
     --network-id=50155 \
     --p2p-interface=$(hostname -i)
